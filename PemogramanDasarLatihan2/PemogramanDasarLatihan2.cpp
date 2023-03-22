d110637534bbd4eed6863c5a66007d82a9aa95ca
defarrel <Defarrel>
2023-03-22 17:03:12 +0700

    up

--- PemogramanDasarLatihan2/PemogramanDasarLatihan2.cpp
+++ PemogramanDasarLatihan2/PemogramanDasarLatihan2.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
 	int a[5];
-	cout << "Mengisi array" << endl;
+	cout << "Mengisi Array" << endl;
 	for (int i = 0; i < 5; i++) {
-		cout << "data ke- " << (i+1) << ": ";
+		cout << "data ke- " << (i + 1) << ": ";
 		cin >> a[i];
-
 	}
-	cout << "Membaca array" << endl;
-	for (int i = 0; 1 < 5; i++) {
-		cout << "data ke-" << (i + 1) << a[i] << endl;
+	cout << "Membaca Array" << endl;
+	for (int i = 0; i < 5; i++) {
+		cout << "data ke-" << (i + 1) << ": " << a[i] << endl;
 	}
 }
