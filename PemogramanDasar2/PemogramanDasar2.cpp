7fcd2197b0d41dc22eba6dfbe51c8e1161ea7d9b
defarrel <Defarrel>
2023-03-22 16:00:15 +0700

    looping

--- PemogramanDasar2/PemogramanDasar2.cpp
+++ PemogramanDasar2/PemogramanDasar2.cpp
@@ -5,4 +5,59 @@ float luaspersegi(float p, float l) {
     return p * l;
 }
 
-float luaslingkaran(float r)
+float luaslingkaran(float r) {
+    return 3.14 * r * r;
+}
+
+float luassegitiga(float a, float t) {
+    return 0, 5 * a * t;
+}
+
+int main(){
+
+    float panjang,lebar, tinggi, jejari, alas;
+    int pilihan;
+  
+    do {
+    cout << "\n================" << endl;
+    cout << "======MENU======" << endl;
+    cout << "================" << endl;
+    cout << "1. Luas Persegi" << endl;
+    cout << "2. luas Lingkaran" << endl;
+    cout << "3. Luas Segitiga" << endl;
+    cout << "4. Exit" << endl;
+    cout << "\nMasukan Pilihan (1/2/3/4) : ";
+
+    cin >> pilihan;
+
+    
+        switch (pilihan) {
+        case 1:
+            cout << "Masukan Panjang = ";
+            cin >> panjang;
+            cout << "Masukan Lebar = ";
+            cin >> lebar;
+            cout << "\nLuas Persegipanjang = " << luaspersegi(panjang, lebar);
+            break;
+        case 2:
+            cout << "Masukan jari-jari  = ";
+            cin >> jejari;
+            cout << "\nLuas Lingkaran = " << luaslingkaran(jejari);
+            break;
+        case 3:
+            cout << "Masukan alasnya = ";
+            cin >> alas;
+            cout << "Masukan tingginya = ";
+            cin >> tinggi;
+            cout << "\nLuas Segitiga = " << luassegitiga(alas, tinggi);
+            break;
+        case 4:
+            break;
+        default:
+            cout << "Pilihan salah!";
+            break;
+
+
+        }
+    } while (pilihan != 4);
+}
